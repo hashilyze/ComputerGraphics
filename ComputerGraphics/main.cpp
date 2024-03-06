eab44a6fbce576c0815a997558e5ad2cf426a45a
hashilyze <hashilyze@gmail.com>
2024-03-06 18:49:41 +0900

    초기 커밋.

---
+++ ComputerGraphics/main.cpp
@@ -0,0 +1,25 @@
+#include "gl/glut.h"
+
+void display() {
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(.0f, 1.0f, .0f);
+	
+	glBegin(GL_POLYGON);
+	glVertex2f(-0.5f, -0.5f);
+	glVertex2f(.0f, 0.5);
+	glVertex2f(0.5f, -0.5f);
+	glEnd();
+
+	glFinish();
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutCreateWindow("OpenGL-Triangle");
+	glutDisplayFunc(display);
+	glutMainLoop();
+
+	return 0;
+}
